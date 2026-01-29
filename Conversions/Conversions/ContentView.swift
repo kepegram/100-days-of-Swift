3b81f1b3f1523c6e253eedb2d3baea9e58f6802b
Kadin Pegram <kepegram1@gmail.com>
2026-01-29 10:03:48 -0600

    Add Conversions project (Challenge 1)

---
+++ Conversions/Conversions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Conversions
+//
+//  Created by Kadin Pegram on 1/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Kadin!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
