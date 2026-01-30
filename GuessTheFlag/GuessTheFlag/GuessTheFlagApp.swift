7f185a165a1c6889a16a49480c84298ed0bfca6f
Kadin Pegram <kepegram1@gmail.com>
2026-01-30 09:56:57 -0600

    Day 20: Initialize project 2

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kadin Pegram on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
