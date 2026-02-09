c7fadd6d2b65218a6714c4c1e507f2dd22e5f4be
Kadin Pegram <kepegram1@gmail.com>
2026-02-09 09:50:16 -0600

    Day 23: Init project 3

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Kadin Pegram on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
