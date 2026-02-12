970de4e908ad26f77416ca3661a718df7ec24105
Kadin Pegram <kepegram1@gmail.com>
2026-02-12 09:50:21 -0600

    Day 26: Init project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kadin Pegram on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
