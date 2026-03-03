264dbbe6b548d8c34b1787394a54b4c42b1baf74
Kadin Pegram <kepegram1@gmail.com>
2026-03-03 10:45:35 -0600

    Day 32: Project 6 part 1

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kadin Pegram on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
