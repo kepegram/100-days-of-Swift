f6d46302fc7b17ea720f13f753f2428868499b75
Kadin Pegram <kepegram1@gmail.com>
2026-03-09 10:00:36 -0500

    Day 36: Project 7 part 1 start

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kadin Pegram on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
