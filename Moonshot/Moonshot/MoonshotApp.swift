b707a4410f58eb79a9322acc07fdbbeeb5bbc50f
Kadin Pegram <kepegram1@gmail.com>
2026-03-12 11:21:05 -0500

    Day 39: Project 8 part 1

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kadin Pegram on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
