dc5e1eb4c023cb3f40dd66e120bec1a22d2cf8f6
Kadin Pegram <kepegram1@gmail.com>
2026-03-18 08:48:14 -0500

    Day 43: Project 9 part 1

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Kadin Pegram on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
