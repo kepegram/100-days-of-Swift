fc0defc01fca37793dbd67478eba6594c702d5cf
Kadin Pegram <kepegram1@gmail.com>
2026-03-26 11:56:34 -0500

    Day 49: Project 10 part 1

---
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Kadin Pegram on 3/26/26.
+//
+
+import SwiftUI
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        guard
+            let url = URL(
+                string:
+                    "https://itunes.apple.com/search?term=j+cole&entity=song"
+            )
+        else {
+            print("Invalid url")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(
+                Response.self,
+                from: data
+            ) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kadin Pegram on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
