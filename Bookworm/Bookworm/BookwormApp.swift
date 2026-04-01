443941419d6821d75be5174f4299cce70d34cf71
Kadin Pegram <kepegram1@gmail.com>
2026-04-01 12:52:55 -0500

    Day 53: Project 11 part 1

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kadin Pegram on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
