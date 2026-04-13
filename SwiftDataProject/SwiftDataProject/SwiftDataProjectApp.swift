373e79d91ca0a8c29b354a338971a1bdf8708790
Kadin Pegram <kepegram1@gmail.com>
2026-04-13 10:07:46 -0500

    Day 57: Project 12 init

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Kadin Pegram on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
