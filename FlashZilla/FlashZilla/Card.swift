eb865607e41af994419bca17a00a92718abc9aa0
Kadin Pegram <kepegram1@gmail.com>
2026-06-30 11:08:03 -0500

    Day 89: Project 14 part 4

---
+++ FlashZilla/FlashZilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  FlashZilla
+//
+//  Created by Kadin Pegram on 6/30/26.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
---
+++ FlashZilla/FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by Kadin Pegram on 6/30/26.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
