c25f79480099c19b50c6b702a9f9b4ce10233e81
Kadin Pegram <kepegram1@gmail.com>
2026-07-13 12:25:20 -0500

    Day 96: Project 19 start

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Kadin Pegram on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
